a390ed997febd076c3b7199674e94be55f007aba
djonnyx <djonnyx@gmail.com>
2020-06-22 22:25:10 +0300

    refactor: code review

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,4 @@
 export const environment = {
   production: false,
-  apiConfig: {
-    host: 'http://127.0.0.1:2222',
-    routes: {
-      api: {
-        baseUrl: 'v1',
-        login: 'login',
-        registration: 'registration',
-        tickets: 'tickets',
-        tarifs: 'tarifs',
-        categories: 'categories',
-      }
-    }
-  }
+  apiConfig: {}
 };
