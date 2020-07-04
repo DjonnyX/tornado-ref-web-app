1b45adf3675dd0596dced3b0c744391163d57259
djonnyx <djonnyx@gmail.com>
2020-07-04 16:45:18 +0300

    refactor: improved node-tree styles

--- src/app/components/state-panel/state-panel.module.ts
+++ src/app/components/state-panel/state-panel.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchFieldModule } from '@components/search-field/search-field.module';
 import { StatePanelComponent } from './state-panel.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { StatePanelComponent } from './state-panel.component';
   imports: [
     CommonModule,
     SearchFieldModule,
+    FlexLayoutModule,
   ]
 })
 export class StatePanelModule { }
