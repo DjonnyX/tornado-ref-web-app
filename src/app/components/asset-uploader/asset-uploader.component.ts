70a7a5e444fbb54f45ffbcf14735088c7a25a865
djonnyx <djonnyx@gmail.com>
2020-07-08 10:43:57 +0300

    feat: created AssetUploader component

---
+++ src/app/components/asset-uploader/asset-uploader.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ta-asset-uploader',
+  templateUrl: './asset-uploader.component.html',
+  styleUrls: ['./asset-uploader.component.scss']
+})
+export class AssetUploaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
