a35e9f33da2e8f8401a0f0aeccae6c799c9f1043
djonnyx <djonnyx@gmail.com>
2020-09-08 09:42:18 +0300

    feat: menu navigation by nativr back button implemented

--- src/app/containers/admin/admin.container.ts
+++ src/app/containers/admin/admin.container.ts
@@ -2,9 +2,9 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 import { IAppState } from '@store/state';
 import { MediaObserver } from '@angular/flex-layout';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, NavigationStart } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map, takeUntil } from 'rxjs/operators';
+import { map, takeUntil, filter } from 'rxjs/operators';
 import { AdminSelectors } from '@store/selectors';
 import { INavRoute } from './interfaces';
 import { AdminActions } from '@store/actions/admin.action';
@@ -73,23 +73,53 @@ export class AdminContainer extends BaseComponent implements OnInit, OnDestroy {
     },
   ];
 
+  private _currentRouteIndex: number;
+
   constructor(private _media: MediaObserver, private _router: Router, private _activatedRoute: ActivatedRoute, private _store: Store<IAppState>) {
     super();
   }
 
+  private extractUrlPath(url: string): string {
+    const urlPath = url?.match(/(?<=(\/admin\/))([\w-])+/);
+    return !!urlPath && urlPath.length > 0 ? urlPath[0] : undefined;
+  }
+
+  private getIndexByRoute(route: string): number {
+    return this.roteCollection.findIndex(v => v.route === route);
+  }
+
   ngOnInit() {
 
+    this._router.events.pipe(
+      filter(event => event instanceof NavigationStart)
+    ).subscribe((event: NavigationStart) => {
+      const url = this.extractUrlPath(event.url);
+      const index = this.getIndexByRoute(url);
+      
+      if (index > -1 && this._currentRouteIndex !== index) {
+        this._store.dispatch(AdminActions.setCurrentRouteIndex({ currentRouteIndex: index }));
+      }
+    });
+
     this.sidenavIsOpen$ = this._store.pipe(
-      select(AdminSelectors.selectSidenavIsOpen)
+      select(AdminSelectors.selectSidenavIsOpen),
     );
 
     this.currentRouteIndex$ = this._store.pipe(
-      select(AdminSelectors.selectCurrentRouteIndex)
+      select(AdminSelectors.selectCurrentRouteIndex),
     );
+
+    this.currentRouteIndex$.pipe(
+      takeUntil(this.unsubscribe$),
+    ).subscribe(index => {
+      this._currentRouteIndex = index;
+    });
 
     this.currentRoute$ = this.currentRouteIndex$.pipe(
       map(v => this.roteCollection[v]),
     );
+
+    this.currentRouteIndex$.subscribe(v => console.log(v))
 
     this.isMobile$ = this._media.media$.pipe(
       map(v => v.suffix === 'Xs')
