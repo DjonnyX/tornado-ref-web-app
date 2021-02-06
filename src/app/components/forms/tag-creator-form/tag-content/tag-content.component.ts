2093f0762e5baed95950d241d2fc1f2144fd4133
djonnyx <djonnyx@gmail.com>
2021-02-07 00:29:09 +0300

    feat: tag view mode implemented

--- src/app/components/forms/tag-creator-form/tag-content/tag-content.component.ts
+++ src/app/components/forms/tag-creator-form/tag-content/tag-content.component.ts
@@ -6,6 +6,13 @@ import { BaseComponent } from '@components/base/base-component';
 import { IFileUploadEntityEvent } from '@app/models/file-upload-event.model';
 import { deepMergeObjects } from '@app/utils/object.util';
 import { isEqualWithDefault } from '@app/utils/entity.util';
+import { IKeyValue } from '@components/key-value/key-value.component';
+
+interface IData {
+  name: IKeyValue;
+  description: IKeyValue;
+  color: IKeyValue;
+}
 
 @Component({
   selector: 'ta-tag-content',
@@ -33,6 +40,8 @@ export class TagContentComponent extends BaseComponent implements OnInit, OnDest
 
   @Input() isEditMode: boolean;
 
+  @Input() isEdit: boolean;
+
   @Input() isDefault: boolean;
 
   @Input() defaultContent: ITagContentsItem;
@@ -50,6 +59,8 @@ export class TagContentComponent extends BaseComponent implements OnInit, OnDest
     this._content = content;
 
     this._state = { ...content };
+
+    this.generateData();
 
     this.ctrlName.setValue(content.name);
     this.ctrlDescription.setValue(content.description);
@@ -76,8 +87,35 @@ export class TagContentComponent extends BaseComponent implements OnInit, OnDest
 
   @Output() save = new EventEmitter<void>();
 
+  private _data: IData;
+
+  get data() {
+    return this._data;
+  }
+
   constructor() {
     super();
+  }
+
+  private generateData(): void {
+    if (!this._state) {
+      return;
+    }
+
+    this._data = {
+      name: {
+        key: "Название",
+        value: this._state?.name || ' ---',
+      },
+      description: {
+        key: "Описание",
+        value: this._state?.description || ' ---',
+      },
+      color: {
+        key: "Цвет",
+        value: this._state?.color || ' ---',
+      },
+    };
   }
 
   ngOnInit(): void {
