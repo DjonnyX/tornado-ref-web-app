a09a02daebf900bab4efc48efaa4bcbb57fb1114
Yamogoo <grebennikovmisha@mail.ru>
2021-05-19 11:37:27 +0300

    feature#207: search.component

--- src/app/components/state-panel/state-panel.module.ts
+++ src/app/components/state-panel/state-panel.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchFieldModule } from '@components/search-field/search-field.module';
+import { SearchModule } from '@components/search/search.module';
 import { StatePanelComponent } from './state-panel.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
@@ -15,6 +16,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     CommonModule,
     SearchFieldModule,
     FlexLayoutModule,
+    SearchModule,
   ]
 })
 export class StatePanelModule { }
