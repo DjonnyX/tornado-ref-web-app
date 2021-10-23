f9e47557b6ae0c962da5bcb26f10931d56dfde85
djonnyx <djonnyx@gmail.com>
2021-10-23 23:07:05 +0300

    hotfix: fixed translations of server errors

--- src/app/utils/error.util.ts
+++ src/app/utils/error.util.ts
@@ -2,13 +2,13 @@ import { LocalizationService } from "@app/services/localization/localization.ser
 
 export const localizeError = (message: string, localization: LocalizationService): string => {
     let result = "";
-    if (!message || message === "Token is empty.") {
+    if (!message || message.indexOf("Token is empty.") === 0) {
         result = "Вы не аторизовались.";
-    } else if (message === "jwt expired") {
+    } else if (message.indexOf("jwt expired") === 0) {
         result = "Время сессии истекло.";
-    } else if (message === "token paiload is fail.") {
+    } else if (message.indexOf("token paiload is fail.") === 0) {
         result = "Что-то пошло не так, повторите попытку.";
-    } else if (message === "License method not allowed") {
+    } else if (message.indexOf("License method not allowed") === 0) {
         result = "Метод не доступен.";
     } else {
         result = message;
