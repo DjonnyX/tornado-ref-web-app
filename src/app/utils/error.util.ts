db72acc005c35156556e7fe7bce47190ac617d78
djonnyx <djonnyx@gmail.com>
2021-11-09 01:53:17 +0300

    refactor: code review

--- src/app/utils/error.util.ts
+++ src/app/utils/error.util.ts
@@ -1,21 +1,17 @@
 import { LocalizationService } from "@app/services/localization/localization.service";
 
 export const localizeError = (message: string, localization: LocalizationService): string => {
-    console.log(message, message.indexOf("504 Gateway Timeout"))
-    let result = "";
     if (!message || message.indexOf("Token is empty.") === 0) {
-        result = "Вы не аторизовались.";
+        return "Вы не аторизовались.";
     } else if (message.indexOf("jwt expired") === 0) {
-        result = "Время сессии истекло.";
+        return "Время сессии истекло.";
     } else if (message.indexOf("token paiload is fail.") === 0) {
-        result = "Что-то пошло не так, повторите попытку.";
+        return "Что-то пошло не так, повторите попытку.";
     } else if (message.indexOf("License method not allowed") === 0) {
-        result = "Метод не доступен.";
+        return "Метод не доступен.";
     } else if (message.indexOf("Gateway Timeout") > - 1) {
-        result = "Сервер не отвечает. Попробуйте повторить запроз позже.";
-    } else {
-        result = message;
+        return "Сервер не отвечает. Попробуйте повторить запроз позже.";
     }
 
-    return result;
+    return "";
 }
