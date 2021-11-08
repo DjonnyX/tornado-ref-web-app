22ac7403ea1e9ef3e7bb86db694bc93f254443ad
djonnyx <djonnyx@gmail.com>
2021-11-08 23:03:12 +0300

    feat: added Gateway Timeout translation error

--- src/app/utils/error.util.ts
+++ src/app/utils/error.util.ts
@@ -1,6 +1,7 @@
 import { LocalizationService } from "@app/services/localization/localization.service";
 
 export const localizeError = (message: string, localization: LocalizationService): string => {
+    console.log(message, message.indexOf("504 Gateway Timeout"))
     let result = "";
     if (!message || message.indexOf("Token is empty.") === 0) {
         result = "Вы не аторизовались.";
@@ -10,6 +11,8 @@ export const localizeError = (message: string, localization: LocalizationService
         result = "Что-то пошло не так, повторите попытку.";
     } else if (message.indexOf("License method not allowed") === 0) {
         result = "Метод не доступен.";
+    } else if (message.indexOf("Gateway Timeout") > - 1) {
+        result = "Сервер не отвечает. Попробуйте повторить запроз позже.";
     } else {
         result = message;
     }
